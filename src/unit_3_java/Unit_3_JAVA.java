817670d8d0d3ed0d3c8a29da28ac3f2c035dd65f
Sarah <Sarah@HP-SARAHI>
2024-06-20 03:13:47 -0600

    Se agrega

---
+++ src/unit_3_java/Unit_3_JAVA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unit_3_java;
+
+import Forms.Frm_app;
+
+/**
+ *
+ * @author Sarah
+ */
+public class Unit_3_JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Frm_app().setVisible(true);
+    }
+    
+}
